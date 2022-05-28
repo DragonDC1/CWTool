c2d55f02a488fec3fc512507e124f4324412ee26
DragonDC_ <finnegankaiser@gmail.com>
2022-05-28 19:20:00 +0200

    Test

--- src/de/fida/cwtool/gui/Window.java
+++ src/de/fida/cwtool/gui/Window.java
@@ -30,6 +30,8 @@ public class Window {
 
         loadImages();
 
+        //Zest
+
         frame = new JFrame(TITLE);
         frame.setSize(WIDTH, HEIGHT);
         frame.setLocationRelativeTo(null);
